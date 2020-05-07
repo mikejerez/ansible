71e44319e6c2816b02bd58220720ad85860d84bc
mikejerez <mikejerez1@hotmail.com>
2020-05-07 16:43:43 -0400

    Update ConfigureRemotingForAnsible.ps1

--- ConfigureRemotingForAnsible.ps1
+++ ConfigureRemotingForAnsible.ps1
@@ -448,8 +448,8 @@ Else
     Write-Log "Unable to establish an HTTP or HTTPS remoting session."
     Throw "Unable to establish an HTTP or HTTPS remoting session."
 }
+winrm set winrm/config/service/auth '@{Basic="true"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 Write-VerboseLog "PS Remoting has been successfully configured for Ansible."
 
-winrm set winrm/config/service/auth '@{Basic="true"}'
-
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
